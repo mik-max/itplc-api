29361ea0772aefe42508fa817f374ee90240b54b
Michael chinye <michaelchinye2018@gmail.com>
2023-04-06 18:51:02 +0100

    configured homepage api to get all videos uploaded

--- src/data/home.js
+++ src/data/home.js
@@ -2,7 +2,7 @@ import sanityClient from '@sanity/client';
 import configData from '../../config.js';
 const client = sanityClient(configData.sanity)
 export const getHomePageData = async () => {
-     const query = '*[_type=="homePage"][0]{"videoUrl": video.asset->url, title}'
+     const query = '*[_type=="homePage"]{"videoUrl": video.asset->url, title}'
      let res = client.fetch(query, {}).then((data) => {
           return data
      })
